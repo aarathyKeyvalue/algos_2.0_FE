c75d26438295275d65dcf9e87c11506ca88ced31
aarathyKeyvalue <aarathy@keyvalue.systems>
2023-09-15 09:56:31 +0530

    Index

---
+++ src/js/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './styles.scss';
+
+const Header = (props) => {
+  const { hasSearch, hasCart} = props;
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  )
+}
