91879251c804e3de05b148d6efa8709d82b20ed9
Nikhin <nikhin@keyvalue.systems>
2023-09-15 11:09:04 +0530

    feat: Adding files

--- src/js/containers/shop/Shop.tsx
+++ src/js/containers/shop/Shop.tsx
@@ -1,8 +1,44 @@
 import React from "react";
+import Category from "app/components/category/Category";
 import styles from "./styles.scss";
+import { Box, Button } from "@mui/material";
+import muiStyles from "./styles";
+import categories from "./categories";
 
 const Shop = () => {
-  return <div className={styles.container}>hi</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.mainTab}>
+        <Button
+          variant="contained"
+          disableElevation
+          sx={[muiStyles.button, muiStyles.primary]}
+          onClick={() => null}
+        >
+          Equipments
+        </Button>
+        <Button
+          variant="contained"
+          disableElevation
+          sx={[muiStyles.button, muiStyles.secondary]}
+          onClick={() => null}
+        >
+          Equipments
+        </Button>
+      </div>
+      <div className={styles.category}>
+        <div className={styles.sectionTitle}>Purchase by category</div>
+        <div className={styles.categories}>
+          {categories.map((category) => (
+            <div style={{ marginRight: 20 }}>
+
+            <Category image={category.image} size={60} label={category.label} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
---
+++ src/js/containers/shop/categories.ts
@@ -0,0 +1,35 @@
+const categories = [
+  {
+    label: "Kits",
+  },
+  {
+    label: "Accessories",
+    image:
+      "https://myborosil.com/cdn/shop/files/my-borosil-borosilicate-glass-bottles-550-ml-neo-borosilicate-glass-bottle-silver-lid-32349056630922.jpg",
+  },
+  {
+    label: "Seeds",
+    image:
+      "https://5.imimg.com/data5/ANDROID/Default/2023/2/CI/PI/ZP/4421857/product-jpeg.jpg",
+  },
+  {
+    label: "Nutrients",
+    image:
+      "https://myborosil.com/cdn/shop/files/my-borosil-borosilicate-glass-bottles-550-ml-neo-borosilicate-glass-bottle-silver-lid-32349056630922.jpg?v=1686031737",
+  },
+  {
+    label: "GrowLight",
+  },
+  {
+    label: "Kits",
+    image:
+      "https://5.imimg.com/data5/ANDROID/Default/2023/2/CI/PI/ZP/4421857/product-jpeg.jpg",
+  },
+  {
+    label: "Seeds",
+    image:
+      "https://5.imimg.com/data5/ANDROID/Default/2023/2/CI/PI/ZP/4421857/product-jpeg.jpg",
+  },
+];
+
+export default categories;
