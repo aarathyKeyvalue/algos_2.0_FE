a73e37b981488108ace90ff6b01e0a2327a60927
Jisha Varghese <jisha.v@keyvalue.systems>
2023-09-16 01:58:48 +0530

    post api and typo correction

--- src/js/containers/shop/apiSlice.ts
+++ src/js/containers/shop/apiSlice.ts
@@ -4,10 +4,34 @@ const shopeApis = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getCategories: builder.query({
       query: () => ({
-        url: "/products/categories"
+        url: "/products/categories",
+      }),
+    }),
+    likePost: builder.mutation({
+      query: (params) => ({
+        url: `/posts/${params.postId}/like/${params.userId}`,
+        method: "PATCH",
+      }),
+    }),
+    unlikePost: builder.mutation({
+      query: (params) => ({
+        url: `/posts/${params.postId}/unlike/${params.userId}`,
+        method: "PATCH",
+      }),
+    }),
+    createPost: builder.mutation({
+      query: (data) => ({
+        url: `/posts/create`,
+        method: "POST",
+        body: data,
       }),
     }),
   }),
 });
 
-export const { useGetCategoriesQuery } = shopeApis;
+export const {
+  useGetCategoriesQuery,
+  useLikePostMutation,
+  useUnlikePostMutation,
+  useCreatePostMutation,
+} = shopeApis;
