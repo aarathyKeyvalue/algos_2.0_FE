6795a87a87e13ae14f350dfaa8652c4117a6b852
Nikhin <nikhin@keyvalue.systems>
2023-09-15 11:16:29 +0530

    feat: Adding footer

--- src/js/layouts/main-layout/MainLayout.tsx
+++ src/js/layouts/main-layout/MainLayout.tsx
@@ -1,18 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 import {
   HashRouter as Router,
   Routes,
   Route,
-  Navigate
-} from 'react-router-dom';
-import Shop from 'app/containers/shop/Shop';
+  Navigate,
+} from "react-router-dom";
+import Shop from "app/containers/shop/Shop";
 
-import styles from './styles.scss';
-import HomeLayout from './HomeLayout';
+import styles from "./styles.scss";
+import HomeLayout from "./HomeLayout";
+import Header from "app/components/header/Header";
 
-const MainLayout:FC<object> = () => {
+const MainLayout: FC<object> = () => {
   return (
     <div className={styles.mainLayoutWrapper}>
+      <Header hasMenu titleCenter hasSearch title="Shop" />
       <Router>
         <Routes>
           <Route path="" element={<Navigate to="/home" />} />
